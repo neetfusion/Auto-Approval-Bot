ffa325e52f95e15dd038be8e9e6a1a05f3e41fec
neetfusion <neetfusion.in@gmail.com>
2025-08-21 16:04:50 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 from os import getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20793620"))
-    API_HASH = getenv("API_HASH", "a712d2b8486f26c4dee5127cc9ae0615")
-    BOT_TOKEN = getenv("BOT_TOKEN", "8145605245:AAHq3JpOz1-5JyObbIsWYlKbBt_bZMuBIEo")
+    API_ID = int(getenv("API_ID", "27846034"))
+    API_HASH = getenv("API_HASH", "980caee71c20f6babaf86d985f5af9e5")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7976191427:AAFYxlIWAaDPPBeYBwgh3GKT1ysfUP_ceGI")
     
     # Your Force Subscribe Channel Id
-    CHID = int(getenv("CHID", "-1002581144687"))  # Make the bot admin in this channel
+    CHID = int(getenv("CHID", "-1002220587356"))  # Make the bot admin in this channel
     
     # Bot owners (use space-separated user IDs in env)
-    SUDO = list(map(int, getenv("SUDO", "6853851676").split()))
+    SUDO = list(map(int, getenv("SUDO", "1320989352").split()))
     
     # MongoDB connection URI
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://pokemonchannel098:yaE7BvFwWIXdb3HQ@cluster0.gdr57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://neetfusionin:neetfusionin@cluster0.c7aecmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 cfg = Config()
